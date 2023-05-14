33770e018a455972dd984c98a009077399258acb
yoojeongsik <halfbasement@innerwave.co.kr>
2023-05-14 16:23:44 +0900

    설명추가

--- spring-boot/embed/src/main/java/hello/boot/MySpringBootApplication.java
+++ spring-boot/embed/src/main/java/hello/boot/MySpringBootApplication.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.ComponentScan;
 
 import java.lang.annotation.*;
 
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)//어디에 이 에노테이션을 붙일 수 있는지
+@Retention(RetentionPolicy.RUNTIME) // 애노테이션이 언제까지 살아 남아 있을지를 정하는 것 ( 라이프사이클 )
 @Documented
 @ComponentScan
 public @interface MySpringBootApplication {
