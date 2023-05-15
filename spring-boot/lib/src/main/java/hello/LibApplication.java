5aecb9903963e814cb6a5a7a52f40350c4ac3caa
yoojeongsik <halfbasement@innerwave.co.kr>
2023-05-15 10:02:24 +0900

    주석추가

--- spring-boot/lib/src/main/java/hello/LibApplication.java
+++ spring-boot/lib/src/main/java/hello/LibApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LibApplication {
 
+
+    //main
     public static void main(String[] args) {
         SpringApplication.run(LibApplication.class, args);
     }
