5c2a27c18e199c847f84237f2a56fd7bac117a86
yoojeongsik <halfbasement@innerwave.co.kr>
2023-05-21 15:24:05 +0900

    test code

--- spring-boot/springboot-developer/src/test/java/com/sik/springbootdeveloper/test/TestControllerTest.java
+++ spring-boot/springboot-developer/src/test/java/com/sik/springbootdeveloper/test/TestControllerTest.java
@@ -2,15 +2,23 @@ package com.sik.springbootdeveloper.test;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc //MockMvc생성&자동구성 / 테스트용 MVC 환경을 만들어 테스트 ( 컨트롤러 테스트 )
 @SpringBootTest
@@ -33,6 +41,27 @@ class TestControllerTest {
     @AfterEach
     public void cleanUp(){
         memberRepository.deleteAll();
+    }
+
+
+    @DisplayName("getAllMembers: 아티클 조회에 성공한다.")
+    @Test
+    public void getAllMembers() throws Exception{
+
+        //given
+        final String url = "/test";
+        Member member = memberRepository.save(new Member(1L, "유정식"));
+
+        //when
+        ResultActions result = mockMvc.perform(get(url).accept(MediaType.APPLICATION_JSON));
+
+
+        //result
+        result
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(member.getId()))
+                .andExpect(jsonPath("$[0].name").value(member.getName()));
+
     }
 
 
