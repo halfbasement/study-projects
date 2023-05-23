6df7d19eaad9b4d3953ff870383d151aa446e7fc
sik <vnfthr@naver.com>
2023-05-24 00:49:03 +0900

    5

--- java/java/sik/ch6/ex/Ex5.java
+++ java/java/sik/ch6/ex/Ex5.java
@@ -2,4 +2,53 @@ package sik.ch6.ex;
 
 public class Ex5 {
 
+    public static void main(String[] args) {
+
+        Student5 s = new Student5("홍길동", 1, 1, 100, 60, 76);
+
+        System.out.println(s.info());
+    }
+
+}
+
+class Student5{
+
+
+    public Student5(String name, int ban, int no, int kor, int eng, int math) {
+        this.name = name;
+        this.ban = ban;
+        this.no = no;
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
+    }
+
+    String name;
+    int ban;
+    int no;
+    int kor;
+    int eng;
+    int math;
+
+    public int getTotal(){
+
+        int sum = this.kor + this.eng + this.math;
+
+        return sum;
+    }
+
+    public float getAverage(){
+        int sum = this.kor + this.eng + this.math;
+
+        float avg = sum/3;
+
+
+        return avg;
+    }
+
+
+    public String info(){
+        return name+","+no+","+ban+","+kor+","+eng+","+math+","+this.getTotal()+","+this.getAverage();
+    }
+
 }
---
+++ java/java/sik/ch6/ex/Ex9.java
@@ -0,0 +1,55 @@
+package sik.ch6.ex;
+
+public class Ex9 {
+
+    public static void main(String[] args) {
+
+
+        Marine marine = new Marine();
+
+        marine.weaponUp();
+
+        System.out.println("marine = " + marine);
+
+        Marine marine2 = new Marine();
+
+        System.out.println("marine2 = " + marine2);
+
+    }
+}
+
+class Marine{
+
+    int x=0,y=0;
+
+    int hp = 60;
+     static int weapon =6;
+    private static int armor = 0;
+
+
+    void weaponUp(){
+        weapon++;
+    }
+
+    void armorUp(){
+        armor++;
+    }
+
+    void move(int x, int y){
+        this.x = x;
+        this.y = y;
+
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Marine{");
+        sb.append("x=").append(x);
+        sb.append(", y=").append(y);
+        sb.append(", hp=").append(hp);
+        sb.append(", weapon=").append(weapon);
+        sb.append(", armor=").append(armor);
+        sb.append('}');
+        return sb.toString();
+    }
+}
