0e1ffb2b313e36dd7ce14811391a1620479047d2
sik <vnfthr@naver.com>
2023-06-02 00:16:09 +0900

    ch7생성

---
+++ java/java/sik/ch7/DrawShape.java
@@ -0,0 +1,27 @@
+package sik.ch7;
+
+public class DrawShape {
+}
+
+//재작성
+class Shape{
+
+}
+
+
+class Point{
+    int x;
+    int y;
+
+
+
+}
+
+class Circle extends Shape{
+
+    Point point;
+    int r;
+
+
+
+}
