097f76a6bb8319811de081ec19a2ec13b8dac75c
sik <vnfthr@naver.com>
2023-06-09 00:16:31 +0900

    ch7 st

--- java/java/sik/ch7/DrawShape.java
+++ java/java/sik/ch7/DrawShape.java
@@ -6,6 +6,12 @@ public class DrawShape {
 
 class Shape{
 
+    String color = "black";
+
+    void draw(){
+        System.out.printf("[color=%s]%n",color);
+    }
+
 }
 
 
@@ -13,15 +19,49 @@ class Point{
     int x;
     int y;
 
+    Point (int x , int y){
+        this.x = x;
+        this.y = y;
+    }
 
+    Point(){
+        this(0,0);
+    }
 
+    String getXY(){
+        return "("+x+","+y+")";
+    }
 }
 
 class Circle extends Shape{
 
-    Point point;
+    Point center;
     int r;
 
+    Circle(){
+        this(new Point(0,0),100);
+    }
+
+    Circle(Point center , int r){
+        this.center = center;
+        this.r = r;
+    }
+
+    void draw(){
+        System.out.printf("[center=(%d,%d), r=%d, color=%s]%n",center.x , center.y , r, color);
+    }
 
 
+}
+
+class Triangle extends Shape{
+    Point[] p = new Point[3];
+
+    Triangle(Point[] p ){
+        this.p = p;
+    }
+
+    void draw(){
+        System.out.println("[p1");
+    }
 }
