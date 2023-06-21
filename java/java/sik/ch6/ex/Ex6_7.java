c174d44bffdf112684b24448b4f3b0d67b138d94
sik <vnfthr@naver.com>
2023-06-22 00:56:33 +0900

    연습문제

---
+++ java/java/sik/ch6/ex/Ex6_7.java
@@ -0,0 +1,5 @@
+package sik.ch6.ex;
+
+public class Ex6_7 {
+    // 6_6 ~ 7  거리구하는법X
+}
---
+++ java/java/sik/ch6/ex/ExAll.java
@@ -0,0 +1,15 @@
+package sik.ch6.ex;
+
+public class ExAll {
+
+    //8
+    //클래스변수 : width, height
+    // 인스 : kind , num
+    // 지역 : k , n
+
+    //9
+    // weapon , armor 모든병사의 공격력과 방어력이 같아야하기 때문에
+
+    //10
+    // a,c,d
+}
